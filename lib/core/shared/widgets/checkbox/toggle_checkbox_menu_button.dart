748c52a82c3578672b1b1b9fc3809fd52d1cd3a6
Mohamed Hassan <mohamed7github@gmail.com>
2025-07-23 17:19:05 +0300

    enhance documentation for ToggleCheckboxMenuButton widget

--- lib/core/shared/widgets/checkbox/toggle_checkbox_menu_button.dart
+++ lib/core/shared/widgets/checkbox/toggle_checkbox_menu_button.dart
@@ -1,6 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+/// A widget that displays a toggle checkbox menu button
+/// that uses a [BlocSelector] to determine its state and handle changes.
+/// It allows for a customizable label and is useful for filtering or toggling options in a menu.
+/// It is generic and can be used with any [Cubit] and state type.
+/// [C] is the type of the Cubit, and [S] is the type
+/// of the state managed by the Cubit.
+/// The [selector] function is used to select the boolean value from the state,
+/// and the [onChanged] function is called when the checkbox is toggled.
+/// The [lable] parameter allows you to set a custom label for the checkbox.  
+/// 
 class ToggleCheckboxMenuButton<C extends Cubit<S>, S> extends StatelessWidget {
   const ToggleCheckboxMenuButton({
     super.key,
